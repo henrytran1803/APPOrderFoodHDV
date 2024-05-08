d56a555fa436d96fd568509eaff3ec3240729d33
Trần Việt Anh <tranvietanh1803@gmail.com>
2024-05-08 14:20:51 +0700

    Initial Commit

---
+++ APPOrderFoodHDV/APPOrderFoodHDVApp.swift
@@ -0,0 +1,17 @@
+//
+//  APPOrderFoodHDVApp.swift
+//  APPOrderFoodHDV
+//
+//  Created by Tran Viet Anh on 08/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct APPOrderFoodHDVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
